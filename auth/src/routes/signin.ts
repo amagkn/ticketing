7666679ab43874708a9b8c475f17c1c01625a137
Dmitrii Repnikov <dmitrii.amagkn@gmail.com>
2023-05-25 21:42:18 +0300

    Signin route

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { validateRequest } from "../middlewares/validate-request";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
+import { Password } from "../services/password";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -13,7 +17,30 @@ router.post(
   "/api/users/signin",
   validationRules,
   validateRequest,
-  async (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    const passwordIsValid = await Password.compare(user.password, password);
+
+    if (!passwordIsValid) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    const userJwt = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_KEY!
+    );
+
+    req.session = { jwt: userJwt };
+
+    return res.status(200).send(user);
+  }
 );
 
 export { router as signinRouter };
